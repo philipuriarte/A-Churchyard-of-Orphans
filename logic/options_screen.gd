08d8a1c13478e1222ae5051b3f8fd2ce11c59a75
PhilipU21 <philipuriarte@gmail.com>
2022-07-23 17:41:38 +0800

    Update options_screen.gd

--- logic/options_screen.gd
+++ logic/options_screen.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	pass
 
 
-# Changes scene to titlee_screen
+# Changes scene to title_screen
 func _on_BackButton_pressed() -> void:
 	get_tree().change_scene("res://scenes/title_screen.tscn")
