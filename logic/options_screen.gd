0f6fafd0f758f9279efd5406e8f8f315112da6cf
PhilipU21 <philipuriarte@gmail.com>
2022-07-23 17:39:27 +0800

    Add logic to BackButton pressed signal in options_screen

--- logic/options_screen.gd
+++ logic/options_screen.gd
@@ -2,3 +2,8 @@ extends PanelContainer
 
 func _ready() -> void:
 	pass
+
+
+# Changes scene to titlee_screen
+func _on_BackButton_pressed() -> void:
+	get_tree().change_scene("res://scenes/title_screen.tscn")
