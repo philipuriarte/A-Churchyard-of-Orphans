dbcaf086153486b253363fdba1cc247a435606ae
PhilipU21 <philipuriarte@gmail.com>
2022-07-23 17:19:45 +0800

    Add logic to OptionsButton pressed signal

--- logic/title_screen.gd
+++ logic/title_screen.gd
@@ -3,5 +3,10 @@ extends PanelContainer
 func _ready() -> void:
 	pass
 
+
 func _on_StartButton_pressed() -> void:
 	get_tree().change_scene("res://scenes/game_screen.tscn")
+
+
+func _on_OptionsButton_pressed() -> void:
+	get_tree().change_scene("res://scenes/options_screen.tscn")
