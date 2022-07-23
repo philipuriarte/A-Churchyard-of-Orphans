f10fd88d5dcc61f18b7d4e3b7f74fcce4d5047a4
PhilipU21 <philipuriarte@gmail.com>
2022-07-23 17:17:28 +0800

    Add logic to StartButton pressed signal

--- logic/title_screen.gd
+++ logic/title_screen.gd
@@ -3,6 +3,5 @@ extends PanelContainer
 func _ready() -> void:
 	pass
 
-
 func _on_StartButton_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene("res://scenes/game_screen.tscn")
