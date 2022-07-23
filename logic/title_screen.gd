078d546ddcdea56ebc180c7a07d02a1f5499275a
PhilipU21 <philipuriarte@gmail.com>
2022-07-23 17:22:08 +0800

    Add logic to QuitButton pressed signal

--- logic/title_screen.gd
+++ logic/title_screen.gd
@@ -4,9 +4,16 @@ func _ready() -> void:
 	pass
 
 
+# Changes scene to game_screen
 func _on_StartButton_pressed() -> void:
 	get_tree().change_scene("res://scenes/game_screen.tscn")
 
 
+# Changes scene to option_screen
 func _on_OptionsButton_pressed() -> void:
 	get_tree().change_scene("res://scenes/options_screen.tscn")
+
+
+# Closes the game
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
