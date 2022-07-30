0006034fe17c5be59d761c1e6ff340709b7eb5ac
PhilipU21 <philipuriarte@gmail.com>
2022-07-30 17:22:16 +0800

    Add onready var choices_container

--- logic/game_screen_logic/middle_container.gd
+++ logic/game_screen_logic/middle_container.gd
@@ -1,11 +1,13 @@
 extends VBoxContainer
 
 
-# Constants that contain reusablee scenes
+# Constants that contain reusable scenes
 const LocationName = preload("res://scenes/reused_scenes/location_name.tscn")
 const NarrativeText = preload("res://scenes/reused_scenes/narrative_text.tscn")
 const Choice = preload("res://scenes/reused_scenes/choice.tscn")
 
+# Variables that contain nodes
+onready var choices_container = $ChoicesMargin/ChoicesContainer
 
 func _ready() -> void:
 	pass
