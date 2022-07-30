e49423dd81cd6e0403d380c80eeb13263d55dcaa
PhilipU21 <philipuriarte@gmail.com>
2022-07-30 17:19:46 +0800

    Add constants to contain reusable scenes

--- logic/game_screen_logic/middle_container.gd
+++ logic/game_screen_logic/middle_container.gd
@@ -1,4 +1,11 @@
 extends VBoxContainer
 
+
+# Constants that contain reusablee scenes
+const LocationName = preload("res://scenes/reused_scenes/location_name.tscn")
+const NarrativeText = preload("res://scenes/reused_scenes/narrative_text.tscn")
+const Choice = preload("res://scenes/reused_scenes/choice.tscn")
+
+
 func _ready() -> void:
 	pass
