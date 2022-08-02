032084e0424b64bf48e77a8267c1d435c63a6510
PhilipU21 <philipuriarte@gmail.com>
2022-08-02 18:14:47 +0800

    Add choice_output function

--- logic/game_screen_logic/content.gd
+++ logic/game_screen_logic/content.gd
@@ -9,7 +9,12 @@ var choice_result := 0
 # Variables that contain nodes
 onready var choices_container = $ChoicesMargin/ChoicesContainer
 
+
 func _ready() -> void:
+	pass
+
+
+func choice_output(result: int) -> void:
 	pass
 
 
