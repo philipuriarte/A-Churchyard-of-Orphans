c1a35d48fdfe38bd62cfb4098f1fa7d00af4f008
PhilipU21 <philipuriarte@gmail.com>
2022-08-03 00:08:34 +0800

    Remove unused code in game_screen.gd

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -1,18 +1,5 @@
 extends PanelContainer
 
-# Variables that contain scenes to instance
-const NarrativeText = preload("res://scenes/reused_scenes/narrative_text.tscn")
-const Choice = preload("res://scenes/reused_scenes/choice.tscn")
-
-# Variables that contain nodes
-onready var content_con = $MarginContainer/MainCointainer/ContentContainer
-onready var choices_con = $MarginContainer/MainCointainer/ChoicesMargin/ChoicesContainer
-onready var narr_text = $MarginContainer/MainCointainer/ContentContainer/NarrativeText
-
-
-func _ready() -> void:
-	pass
-
 
 func _on_TitleButton_pressed() -> void:
 	get_tree().change_scene("res://scenes/title_screen.tscn")
