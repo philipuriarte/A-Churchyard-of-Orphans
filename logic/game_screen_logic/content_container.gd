e12dd15bce0e2e88a7b7af31b59b6de193df7103
PhilipU21 <philipuriarte@gmail.com>
2022-08-02 17:36:09 +0800

    Make slight changes to content_container.gd

--- logic/game_screen_logic/content_container.gd
+++ logic/game_screen_logic/content_container.gd
@@ -1,10 +1,8 @@
 extends VBoxContainer
 
-
 # Constants that contain reusable scenes
 const LocationName = preload("res://scenes/reused_scenes/location_name.tscn")
 const NarrativeText = preload("res://scenes/reused_scenes/narrative_text.tscn")
-const Choice = preload("res://scenes/reused_scenes/choice.tscn")
 
 var choice_result := 0
 
