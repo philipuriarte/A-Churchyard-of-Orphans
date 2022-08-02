1396620ef976391a413bba3908c8b1debd1ee127
PhilipU21 <philipuriarte@gmail.com>
2022-08-02 17:25:17 +0800

    Add choice_result var

--- logic/game_screen_logic/content_container.gd
+++ logic/game_screen_logic/content_container.gd
@@ -6,6 +6,8 @@ const LocationName = preload("res://scenes/reused_scenes/location_name.tscn")
 const NarrativeText = preload("res://scenes/reused_scenes/narrative_text.tscn")
 const Choice = preload("res://scenes/reused_scenes/choice.tscn")
 
+var choice_result := 0
+
 # Variables that contain nodes
 onready var choices_container = $ChoicesMargin/ChoicesContainer
 
