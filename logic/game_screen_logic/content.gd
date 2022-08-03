7473c77b163469880c0321be7ef5aec354d11396
PhilipU21 <philipuriarte@gmail.com>
2022-08-03 16:41:15 +0800

    Add to content_dict

--- logic/game_screen_logic/content.gd
+++ logic/game_screen_logic/content.gd
@@ -17,10 +17,37 @@ var content_dict : Dictionary = {
 		"narr_text": "You wake up to the sudden knock to your room.",
 		"choices": {
 			1: {
-				"text": "Open the door"
+				"text": "Open the door",
+				"output": "open_door"
 			},
 			2: {
-				"text": "Ask who it is"
+				"text": "Ask who it is",
+				"output": "ask_who"
+			}
+		}
+	},
+	"open_door": {
+		"narr_text": "You open the door and find a servant.",
+		"choices": {
+			1: {
+				"text": "Test1"
+			},
+			2: {
+				"text": "Test2"
+			},
+			3: {
+				"text": "Test3"
+			}
+		}
+	},
+	"ask_who": {
+		"narr_text": "You ask who is it. A servant replies to you",
+		"choices": {
+			1: {
+				"text": "Test1"
+			},
+			2: {
+				"text": "Test2"
 			}
 		}
 	}
