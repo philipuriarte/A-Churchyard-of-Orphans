3db530c955964c7e27323752a8460ba65964455c
PhilipU21 <philipuriarte@gmail.com>
2022-08-04 14:32:44 +0800

    Add content_dict.gd

---
+++ logic/content_dict.gd
@@ -0,0 +1,60 @@
+extends Resource
+
+class_name ContentDict
+
+# Dictionary that contains all the text content of the game
+var content_dict := {
+	"prologue": {
+		"narr_text": "You are the son/daughter of Lord *insert name* of *insert place*",
+		"choices": {
+			1: {
+				"text": "Continue",
+				"output": "intro"
+			}
+		}
+	},
+	"intro": {
+		"narr_text": "You wake up to the sudden knock to your room.",
+		"choices": {
+			1: {
+				"text": "Open the door",
+				"output": "open_door"
+			},
+			2: {
+				"text": "Ask who it is",
+				"output": "ask_who"
+			}
+		}
+	},
+	"open_door": {
+		"narr_text": "You open the door and find a servant.",
+		"choices": {
+			1: {
+				"text": "Test1"
+			},
+			2: {
+				"text": "Test2"
+			},
+			3: {
+				"text": "Test3"
+			}
+		}
+	},
+	"ask_who": {
+		"narr_text": "You ask who is it. A servant replies to you",
+		"choices": {
+			1: {
+				"text": "Test1"
+			},
+			2: {
+				"text": "Test2"
+			},
+			3: {
+				"text": "Test3"
+			},
+			4: {
+				"text": "Test4"
+			}
+		}
+	}
+}
