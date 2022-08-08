f1c192ea9c728adb933fa3c2a4c83d3efd985210
PhilipU21 <philipuriarte@gmail.com>
2022-08-08 18:10:55 +0800

    Add ignore warning comments in options_screen.gd

--- logic/options_screen.gd
+++ logic/options_screen.gd
@@ -6,4 +6,5 @@ func _ready() -> void:
 
 # Changes scene to title_screen
 func _on_BackButton_pressed() -> void:
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
