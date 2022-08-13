cde401baaefaba934b901d355b446ec148ab483a
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 16:52:58 +0800

    Add comments to popup_menu.gd

--- logic/popup_menu.gd
+++ logic/popup_menu.gd
@@ -1,10 +1,12 @@
 extends Popup
 
 
+# Changes scene to title_screen
 func _on_TitleButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
 
 
+# Close the Popup Menu
 func _on_ContinueButton_pressed() -> void:
 	hide()
