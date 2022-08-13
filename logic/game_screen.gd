05631ecd070c24d7203388c7ae8fcd187f72137b
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 15:09:22 +0800

    Add comment to game_screen.gd

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 onready var popup_menu: PopupPanel = $"%PopupMenu"
 
 
+# Will open and close the PopupMenu node
 func _on_MenuButton_toggled(button_pressed: bool) -> void:
 	if button_pressed:
 		popup_menu.popup_centered()
