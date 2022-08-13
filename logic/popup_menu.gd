14c17003904a6a0f3468391a892620b762f66143
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 14:35:16 +0800

    Add comments to popup_menu.gd

--- logic/popup_menu.gd
+++ logic/popup_menu.gd
@@ -1,10 +1,12 @@
 extends PopupPanel
 
 
+# Changes scene to title_screen
 func _on_TitleButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
 
 
+# Closes the game
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
