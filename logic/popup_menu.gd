9962c569d0a6a83c55ef2f1a49020f58fd6e3196
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 14:34:33 +0800

    Add pressed signal to QuitButton and code to quit game

--- logic/popup_menu.gd
+++ logic/popup_menu.gd
@@ -4,3 +4,7 @@ extends PopupPanel
 func _on_TitleButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
