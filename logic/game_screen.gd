b9997cdbecf2378ff3bc4925bc36450c09ad6459
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 15:08:44 +0800

    Connect new toggle signal to MenuButton and add code

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -2,5 +2,9 @@ extends PanelContainer
 
 onready var popup_menu: PopupPanel = $"%PopupMenu"
 
-func _on_MenuButton_pressed() -> void:
-	popup_menu.popup_centered()
+
+func _on_MenuButton_toggled(button_pressed: bool) -> void:
+	if button_pressed:
+		popup_menu.popup_centered()
+	else:
+		popup_menu.hide()
