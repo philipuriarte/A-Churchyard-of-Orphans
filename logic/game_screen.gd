9ef99ca4e12be062f887dc06baf60a323715eb36
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 14:52:22 +0800

    Update code on func _on_MenuButton_pressed

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -3,4 +3,4 @@ extends PanelContainer
 onready var popup_menu: PopupPanel = $"%PopupMenu"
 
 func _on_MenuButton_pressed() -> void:
-	pass # Replace with function body.
+	popup_menu.popup_centered()
