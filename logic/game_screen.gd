9431cfba0ff88653937974aeb5e645a9c4d78b51
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 16:51:38 +0800

    Remove toggle from MenuButton and connect new pressed signal

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -3,9 +3,6 @@ extends PanelContainer
 onready var popup_menu: Popup = $"%PopupMenu"
 
 
-# Will open and close the PopupMenu node
-func _on_MenuButton_toggled(button_pressed: bool) -> void:
-	if button_pressed:
-		popup_menu.popup_centered()
-	else:
-		popup_menu.hide()
+# Will open the PopupMenu node
+func _on_MenuButton_pressed() -> void:
+	popup_menu.popup_centered()
