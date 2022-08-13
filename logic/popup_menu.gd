5d1dfed57d3c1bc3b0410c638a8dd4f616309bd2
PhilipU21 <philipuriarte@gmail.com>
2022-08-13 16:40:00 +0800

    Add signal and code to ContinueButton

--- logic/popup_menu.gd
+++ logic/popup_menu.gd
@@ -4,3 +4,7 @@ extends Popup
 func _on_TitleButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
+
+
+func _on_ContinueButton_pressed() -> void:
+	hide()
