6c0e1eef78dfd7f50894fe01d6635bd0ccbb7a38
PhilipU21 <philipuriarte@gmail.com>
2022-08-15 19:07:10 +0800

    Set up credits popup in options_screen

--- logic/options_screen.gd
+++ logic/options_screen.gd
@@ -1,10 +1,30 @@
 extends PanelContainer
 
+onready var popup_credits: Popup = $"%PopupCredits"
+
+
+# Should set options to saved options
 func _ready() -> void:
 	pass
 
 
-# Changes scene to title_screen
+# Open PopupCredits
+func _on_CreditsButton_pressed() -> void:
+	popup_credits.popup_centered()
+
+
+# Close PopupCredits
+func _on_CloseButton_pressed() -> void:
+	popup_credits.hide()
+
+
+# Close PopupCredits if press/click event occurs outside CreditsScreen
+func _on_PopupCredits_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+			popup_credits.hide()
+
+
+# Change scene to title_screen
 func _on_BackButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
