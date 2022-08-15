6fb9f9bfab46ac9001463818148818d317b17014
PhilipU21 <philipuriarte@gmail.com>
2022-08-15 19:25:19 +0800

    Update comments

--- logic/title_screen.gd
+++ logic/title_screen.gd
@@ -8,14 +8,14 @@ func _ready() -> void:
 	continue_btn.visible = false
 
 
-# Changes scene to game_screen
+# Change scene to game_screen
 # Should start new game instead of only changing scene to game_screen
 func _on_NewButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/game_screen.tscn")
 
 
-# Changes scene to option_screen
+# Change scene to option_screen
 func _on_OptionsButton_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/options_screen.tscn")
