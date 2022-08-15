cb25e4ef1eba46720d233c68d0643656f17c0659
PhilipU21 <philipuriarte@gmail.com>
2022-08-15 13:55:01 +0800

    Add code to set function to TopText node in game_screen

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -1,8 +1,14 @@
 extends PanelContainer
 
 onready var popup_menu: Popup = $"%PopupMenu"
+onready var top_text: Label = $"%TopText"
 
 
 # Will open the PopupMenu node
 func _on_MenuButton_pressed() -> void:
 	popup_menu.popup_centered()
+
+
+# Set text to TopText node
+func set_top_text(new_text: String) -> void:
+	top_text.text = new_text
