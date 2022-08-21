f3de379d24ba242375ea0030f761acc8f38be346
PhilipU21 <philipuriarte@gmail.com>
2022-08-22 00:00:21 +0800

    Add function to set text in choice_button.tscn

--- logic/choice_button.gd
+++ logic/choice_button.gd
@@ -2,7 +2,14 @@ extends PanelContainer
 
 export(int, 1, 4) var choice_index: int = 1
 
+onready var choice_text: Label = $"%ChoiceText"
+
 
 # Return the value of choice_index
 func _on_ChoiceButton_pressed() -> int:
 	return choice_index
+
+
+# Set text to ChoiceText
+func set_text(new_text: String) -> void:
+	choice_text.text = new_text
