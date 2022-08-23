8c7305d24b02696a8c3815b5d9df5f7e80443bf7
PhilipU21 <philipuriarte@gmail.com>
2022-08-23 16:38:05 +0800

    Set up code and signals for remaining buttons in options_screen

--- logic/options_screen.gd
+++ logic/options_screen.gd
@@ -8,35 +8,57 @@ onready var test_text: Label = $"%TestText"
 func _ready() -> void:
 	pass
 
+# Set language
+func _on_LangOptions_item_selected(index: int) -> void:
+	match index:
+		0:
+			print("User chose English")
+
+
+# Set music on/off
+func _on_MusicButton_toggled(button_pressed: bool) -> void:
+	if button_pressed:
+		print("User chose to turn on Music")
+	else:
+		print("User chose to turn off Music")
+
+
+# Set sfx on/off
+func _on_SFXButton_toggled(button_pressed: bool) -> void:
+	if button_pressed:
+		print("User chose to turn on SFX")
+	else:
+		print("User chose to turn off SFX")
+
 
 # Set font size
 func _on_SizeOptions_item_selected(index: int) -> void:
 	match index:
 		0:
-			print("User chose 10")
+			print("User chose font size 10")
 		1:
-			print("User chose 11")
+			print("User chose font size 11")
 		2:
-			print("User chose 12")
+			print("User chose font size 12")
 		3:
-			print("User chose 13")
+			print("User chose font size 13")
 		4:
-			print("User chose 14")
+			print("User chose font size 14")
 
 
 # Set line spacing of text
 func _on_LineOptions_item_selected(index: int) -> void:
 	match index:
 		0:
-			print("User chose 10")
+			print("User chose line spacing 10")
 		1:
-			print("User chose 12")
+			print("User chose line spacing 12")
 		2:
-			print("User chose 14")
+			print("User chose line spacing 14")
 		3:
-			print("User chose 16")
+			print("User chose line spacing 16")
 		4:
-			print("User chose 18")
+			print("User chose line spacing 18")
 
 
 # Open PopupCredits
@@ -55,7 +77,12 @@ func _on_PopupCredits_gui_input(event: InputEvent) -> void:
 			popup_credits.hide()
 
 
+# Trigger when user presses ContactButton
+func _on_ContactButton_pressed() -> void:
+	print("User pressed Contact us")
+
+
 # Change scene to title_screen
 func _on_BackButton_pressed() -> void:
-# warning-ignore:return_value_discarded
+	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/title_screen.tscn")
