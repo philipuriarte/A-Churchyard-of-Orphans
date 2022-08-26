69fbe3cfab8853326125a43867f5e3826937a23c
PhilipU21 <philipuriarte@gmail.com>
2022-08-26 22:52:04 +0800

    Update functions to static in save_game.gd

--- logic/save_game.gd
+++ logic/save_game.gd
@@ -15,16 +15,16 @@ func write_savegame() -> void:
 
 
 # Check if a save file exists
-func save_exists() -> bool:
+static func save_exists() -> bool:
 	return ResourceLoader.exists(get_save_path())
 
 
 # Load game
-func load_savegame() -> Resource:
+static func load_savegame() -> Resource:
 	return ResourceLoader.load(get_save_path(), "", true)
 
 
 # Save and load a text resource in debug builds and a binary resource in the released product
-func get_save_path() -> String:
+static func get_save_path() -> String:
 	var extension := ".tres" if OS.is_debug_build() else ".res"
 	return SAVE_GAME_PATH + extension
