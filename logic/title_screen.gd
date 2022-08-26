33eb01d5186ed83c02f5ad14b44983973d27b46f
PhilipU21 <philipuriarte@gmail.com>
2022-08-27 00:03:33 +0800

    Set up new game and continue signals in title_screen

--- logic/title_screen.gd
+++ logic/title_screen.gd
@@ -1,23 +1,41 @@
 extends PanelContainer
 
+signal continue_game
+signal new_game
+
+var save: SaveGame
+
 onready var continue_btn: Button = $"%ContinueButton"
 
 
-# Should set visiblity of Continue button if there is save file or not
+# Set visiblity of Continue button if there is a save file or not
 func _ready() -> void:
-	continue_btn.visible = false
+	if not SaveGame.save_exists():
+		continue_btn.visible = false
 
 
-# Change scene to game_screen
-# Should start new game instead of only changing scene to game_screen
+# Emit continue_game signal and change scene to game_screen
+func _on_ContinueButton_pressed() -> void:
+	emit_signal("continue_game")
+	
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/game_screen.tscn")
+
+
+# Emit new_save signal and change scene to game_screen
 func _on_NewButton_pressed() -> void:
-# warning-ignore:return_value_discarded
+	if SaveGame.save_exists():
+		pass # pop confirmation popup to overwrite save
+	else:
+		emit_signal("new_game")
+	
+	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/game_screen.tscn")
 
 
 # Change scene to option_screen
 func _on_OptionsButton_pressed() -> void:
-# warning-ignore:return_value_discarded
+	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/options_screen.tscn")
 
 
