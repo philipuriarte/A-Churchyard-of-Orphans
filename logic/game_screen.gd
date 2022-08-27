8250cacbabd61df6a42a0e3fa2fb43f7d350e7f9
PhilipU21 <philipuriarte@gmail.com>
2022-08-27 16:30:47 +0800

    Add load functionality to game_screen.gd

--- logic/game_screen.gd
+++ logic/game_screen.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+var save: SaveGame
+
 # Variables that contain nodes
 onready var popup_menu: Popup = $"%PopupMenu"
 onready var top_text: Label = $"%TopText"
@@ -8,11 +10,9 @@ onready var inventory_button: Button = $"%InventoryButton"
 onready var journal_button: Button = $"%JournalButton"
 
 
-# Set BottomContainer buttons visibility to false, will rework in the future
+# Set BottomContainer buttons visibility to false
 func _ready() -> void:
-	character_button.visible = false
-	inventory_button.visible = false
-	journal_button.visible = false
+	load_save()
 
 
 # Open the PopupMenu node
@@ -23,3 +23,12 @@ func _on_MenuButton_pressed() -> void:
 # Set text to TopText node
 func set_top_text(new_text: String) -> void:
 	top_text.text = new_text
+
+
+# Load data of current save file
+func load_save() -> void:
+	save = SaveGame.load_savegame()
+	
+	character_button.visible = save.character_button_visibility
+	inventory_button.visible = save.inventory_button_visibility
+	journal_button.visible = save.journal_button_visibility
