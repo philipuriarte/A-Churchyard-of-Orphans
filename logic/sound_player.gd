b0678c070258b9fa1aab02620cc24ab3d56a8986
PhilipU21 <philipuriarte@gmail.com>
2022-10-01 19:29:02 +0800

    Add sfx to button press

---
+++ logic/sound_player.gd
@@ -0,0 +1,29 @@
+extends Node
+
+onready var button_normal: AudioStreamPlayer = $ButtonNormal
+
+
+func _ready() -> void:
+	connect_buttons(get_tree().root)
+	# warning-ignore:return_value_discarded
+	get_tree().connect("node_added", self, "_on_SceneTree_node_added")
+
+
+func connect_buttons(root):
+	for child in root.get_children():
+		if child is BaseButton:
+			connect_to_button(child)
+		connect_buttons(child)
+
+
+func connect_to_button(button):
+	button.connect("pressed", self, "_on_Button_pressed")
+
+
+func _on_Button_pressed():
+	button_normal.play()
+
+
+func _on_SceneTree_node_added(node):
+	if node is Button:
+		connect_to_button(node)
